76de7cf88f81836ddfbf8a77efe9f91dc5a7441c
taekhyang <xorgid1195@gmail.com>
2021-04-16 17:26:17 +0900

    ADD : user model & initial setting

--- user/models.py
+++ user/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class User(models.Model):
-    email        = models.EmailField(max_length=254, unique=True)
-    password     = models.CharField(max_length=254)
-    corp         = models.CharField(max_length=50)
-    is_verified  = model.models.BooleanField(default=False)
+    email            = models.EmailField(max_length=255, unique=True)
+    password         = models.CharField(max_length=200)
+    corporation_name = models.CharField(max_length=200)
+    is_verified      = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'users'
