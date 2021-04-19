901b066fbcd549894740ad1d084d2e2b733d8768
taekhyang <xorgid1195@gmail.com>
2021-04-19 15:19:31 +0900

    WIP

---
+++ csv_register.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+corporation_df = pd.read_csv('기업정보.csv', dtype=str)
+
+columns = corporation_df.columns.tolist()
+print(columns)
+
+for row in corporation_df.itertuples():
+
+    print(row.cocode)
