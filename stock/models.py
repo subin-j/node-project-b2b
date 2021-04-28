e9f672c2b764ed36c897f4f36214a06d97b52636
jadyJeong <coldfeet0816@gmail.com>
2021-04-28 15:57:37 +0900

    FIX: stockprice model moved to app stock.

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class StockPrice(models.Model):
+    date     = models.DateField()
+    volume   = models.IntegerField()
+    bprc_adj = models.DecimalField(max_digits=20,decimal_places=2)
+    hi_adj   = models.DecimalField(max_digits=20,decimal_places=2)
+    lo_adj   = models.DecimalField(max_digits=20,decimal_places=2)
+    prc_adj  = models.DecimalField(max_digits=20,decimal_places=2)
+    ticker   = models.ForeignKey('corporation.Ticker', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'stock_prices'
+
