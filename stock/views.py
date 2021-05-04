a98ab9159a296ff0b066672e0673343e39acf3d1
taekhyang <xorgid1195@gmail.com>
2021-05-04 13:49:36 +0900

    FIX :fixed candle chart app level logic to db level logic

--- stock/views.py
+++ stock/views.py
@@ -6,9 +6,9 @@ from dateutil.relativedelta  import relativedelta
 from django.http      import JsonResponse
 from django.shortcuts import render
 from django.views     import View
-from django.db.models import F, IntegerField, Subquery, OuterRef
+from django.db.models import F, IntegerField
 from django.db.models.expressions import Window
-from django.db.models.functions import FirstValue, ExtractDay, Cast
+from django.db.models.functions import FirstValue, ExtractDay, Cast, TruncMonth
 from django.db.models.aggregates import Max, Min, Sum
 from django.utils.formats import localize
 
@@ -35,20 +35,7 @@ class StockCandleChart(View):
         
         ticker          = Ticker.objects.get(code=code)
         stock_prices_qs = StockPrice.objects.filter(ticker=ticker, date__gte=two_years_from_now)
-
-        if chart_type == 'daily':
-            stock_prices = [
-                                {
-                                'date'    : str(stock_price_qs.date),
-                                'bprc_adj': stock_price_qs.bprc_adj,
-                                'prc_adj' : stock_price_qs.prc_adj,
-                                'hi_adj'  : stock_price_qs.hi_adj,
-                                'lo_adj'  : stock_price_qs.lo_adj,
-                                'volume'  : stock_price_qs.volume
-                                } for stock_price_qs in stock_prices_qs]
-
-        else:
-            stock_prices = self.get_candle_chart_by_type(chart_type, stock_prices_qs)
+        stock_prices    = self.get_candle_chart_by_type(chart_type, stock_prices_qs)
 
         data = {
                 'name'  : ticker.stock_name,
@@ -59,46 +46,51 @@ class StockCandleChart(View):
         return JsonResponse({'results': data}, status=200)
 
     def get_candle_chart_by_type(self, chart_type, stock_prices_qs):
-        today       = datetime.datetime.today()
-        this_friday = today + datetime.timedelta((calendar.FRIDAY - today.weekday()) % 7)
-        base_date   = this_friday.date()
+        if chart_type == 'daily':
+            results_qs = stock_prices_qs.values('date', 'bprc_adj', 'prc_adj', 'hi_adj', 'lo_adj', 'volume').order_by('date')
+            return list(results_qs)
 
-        first_qs = stock_prices_qs.annotate(group_id=Cast(
-                                     ExtractDay(base_date - F('date')), IntegerField()) / 7)\
-                                     .values('group_id')
-        print(first_qs)
-        
-        second_qs = first_qs.annotate(
-                                        bprc_adj=Window(
-                                            expression   = FirstValue('bprc_adj'),
-                                            partition_by = F('group_id'),
-                                            order_by     = F('date').asc()
-                                        ),
-                                        prc_adj=Window(
-                                            expression   = FirstValue('prc_adj'),
-                                            partition_by = F('group_id'),
-                                            order_by     = F('date').desc()
-                                        ),
-                                        date=Window(
-                                            expression= Max('date'),
-                                            partition_by= F('group_id')
-                                        ),
-                                        hi_adj=Window(
-                                            expression= Max('hi_adj'),
-                                            partition_by=F('group_id')
-                                        ),
-                                        lo_adj=Window(
-                                            expression=Min('lo_adj'),
-                                            partition_by=F('group_id')
-                                        ),
-                                        volume=Window(
-                                            expression=Sum('volume'),
-                                            partition_by=F('group_id')
-                                        )
-                                    )\
-                                    .distinct('date')\
-                                    .values('date', 'bprc_adj', 'prc_adj', 'hi_adj', 'lo_adj', 'volume')
-        print(second_qs.query)
+        if chart_type == 'weekly':
+            today       = datetime.datetime.today()
+            this_friday = today + datetime.timedelta((calendar.FRIDAY - today.weekday()) % 7)
+            base_date   = this_friday.date()
+
+            first_qs = stock_prices_qs.annotate(group_id=Cast(
+                                        ExtractDay(base_date - F('date')), IntegerField()) / 7).order_by('date')
+        elif chart_type == 'monthly':
+             first_qs = stock_prices_qs.annotate(group_id=TruncMonth(F('date'))).order_by('date')
+
+        second_qs = first_qs.values('group_id')\
+                            .annotate(
+                                    bprc_adj=Window(
+                                        expression   = FirstValue('bprc_adj'),
+                                        partition_by = F('group_id'),
+                                        order_by     = F('date').asc()
+                                    ),
+                                    prc_adj=Window(
+                                        expression   = FirstValue('prc_adj'),
+                                        partition_by = F('group_id'),
+                                        order_by     = F('date').desc()
+                                    ),
+                                    date=Window(
+                                        expression   = Max('date'),
+                                        partition_by = F('group_id')
+                                    ),
+                                    hi_adj=Window(
+                                        expression   = Max('hi_adj'),
+                                        partition_by = F('group_id')
+                                    ),
+                                    lo_adj=Window(
+                                        expression   = Min('lo_adj'),
+                                        partition_by = F('group_id')
+                                    ),
+                                    volume=Window(
+                                        expression   = Sum('volume'),
+                                        partition_by = F('group_id')
+                                    )
+                            )\
+                            .distinct('date')\
+                            .values('date', 'bprc_adj', 'prc_adj', 'hi_adj', 'lo_adj', 'volume')
 
         results = list(second_qs)
         return results
